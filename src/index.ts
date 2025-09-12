8c2574a4d2abb586cf380f30c794fc87ca364277
HikariYlan <ylan.nicolas52600@gmail.com>
2025-09-13 01:26:31 +0200

    feat: displaying home page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, {type Request, type Response} from "express";
+import {home_router} from "./home/home.router";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Test")
-})
+app.use(express.static('public'));
+
+app.use(home_router);
 
 app.listen(8000, () => {
     console.log("Website available here: http://localhost:8000");
