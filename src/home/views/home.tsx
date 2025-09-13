374bad6f93730c8052f9f04bec3c632bcc730228
HikariYlan <ylan.nicolas52600@gmail.com>
2025-09-13 02:46:32 +0200

    feat: home page view

---
+++ src/home/views/home.tsx
@@ -0,0 +1,41 @@
+export function HomeView(props: { version: string }): JSX.Element {
+
+    return (<>
+        {'<!DOCTYPE html>'}
+            <head>
+                <title>Outer Wilds Wiki</title>
+                <link rel="stylesheet" href="/styles/app.css"/>
+                <link rel="icon" href="/images/icon.svg"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+                    rel="stylesheet"/>
+            </head>
+            <body class="josefin-sans-font">
+            <div id="main">
+                <video autoplay={true} muted={true} id="background-video">
+                    <source src="/videos/outer_wilds_menu_vid.mp4"/>
+                </video>
+                <div id="logos" class="hidden transition">
+                    <img src="/images/main_logo.png" alt="logo principal"/>
+                    <img src="/images/main_logo_dlc.png" alt="logo DLC"/>
+                </div>
+                <section id="separator" class="hidden transition">
+                    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+                </section>
+                <div id="buttons" class="hidden transition_long">
+                    <button>NOUVELLE EXPÉDITION</button>
+                    <button>OPTIONS</button>
+                    <button>CRÉDITS</button>
+                    <button>CHANGER DE PROFIL</button>
+                </div>
+                <section id="separator2" class="hidden transition">
+                    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+                </section>
+                <p id="version" class="hidden transition">v{props.version}</p>
+            </div>
+            <script src="/scripts/video.js"></script>
+            </body>
+    </>);
+}
