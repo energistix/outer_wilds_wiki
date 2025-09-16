45775c59c5640d8ba2da17f75c4f497ccbc0f322
HikariYlan <ylan.nicolas52600@gmail.com>
2025-09-16 22:58:46 +0200

    feat: credits written

--- src/home/views/credits.tsx
+++ src/home/views/credits.tsx
@@ -2,7 +2,28 @@ export function CreditsView(): JSX.Element {
     return (
         <>
             <dialog class="dialog hidden">
-                <h1>Test</h1>
+                <h1>Crédits</h1>
+                <div id="credits">
+                    <div>
+                        <h1>Outer Wilds</h1>
+                        <p>
+                            <span class="hightlight">Développeur :</span> Mobius Digital
+                        </p>
+                        <p>
+                            <span class="hightlight">Éditeur :</span> Annapurna Interactive
+                        </p>
+                    </div>
+                    <div>
+                        <h1>Outer Wilds Wiki</h1>
+                        <p>
+                            <span class="hightlight">Créateur / maintainer : </span>
+                            <a href="https://github.com/HikariYlan">Hikari / Ylan</a>
+                        </p>
+                        <p>
+                            <a href="https://github.com/HikariYlan/outer_wilds_wiki">Lien vers le projet GitHub</a>
+                        </p>
+                    </div>
+                </div>
                 <button id="closeButton" class="close_dialog">Continuer</button>
             </dialog>
             <script src="/scripts/dialog.js"/>
