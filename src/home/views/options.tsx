58944f3bd5802390dfc2df78b486858f62ee63c0
HikariYlan <ylan.nicolas52600@gmail.com>
2025-09-30 23:11:21 +0200

    style: checkbox

--- src/home/views/options.tsx
+++ src/home/views/options.tsx
@@ -3,12 +3,16 @@ export function OptionsView(): JSX.Element {
         <>
             <dialog class="dialog" open>
                 <h1>Options</h1>
-                <div>
-                    <input id="skipIntroCheck" type="checkbox">Passer l'introduction ?</input>
-                </div>
-                <button id="closeButton" class="close_dialog" onclick="document.querySelector('dialog').remove()">Continuer</button>
+                <label class="checkbox">
+                    <input id="skipIntroCheck" type="checkbox"/>
+                    <span></span>
+                    Passer l'introduction ?
+                </label>
+                <button id="closeButton" class="close_dialog"
+                        onclick="document.querySelector('dialog').remove()">Continuer
+                </button>
                 <script src="/scripts/app.js"/>
-                <script src="/scripts/options.js" />
+                <script src="/scripts/options.js"/>
             </dialog>
         </>
     );
