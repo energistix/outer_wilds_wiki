ad33c3e47c3850f165ae9d0fcf0add16c3da7eb6
HikariYlan <ylan.nicolas52600@gmail.com>
2025-09-30 23:11:48 +0200

    feat: sound for checkboxes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,19 @@
 var buttonAudio = document.getElementById('buttonAudio');
 var buttons = document.querySelectorAll('button');
+var labelAudio = document.getElementById('labelAudio');
+var labels = document.querySelectorAll('label');
 
 // Ajouter un event listener sur chacun
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
         buttonAudio.currentTime = 0; // remet à zéro si déjà en lecture
         buttonAudio.play();
+    });
+});
+
+labels.forEach(lbl => {
+    lbl.addEventListener('click', () => {
+        labelAudio.currentTime = 0; // remet à zéro si déjà en lecture
+        labelAudio.play();
     });
 });
